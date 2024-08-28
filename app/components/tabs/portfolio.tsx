1f6b78cd7be8b79bddea1dba966c98c5f8ea3bcd
yeahimjt <JonathanTrevino1327@yahoo.com>
2024-08-28 02:26:26 -0500

    initialized impression section

--- app/components/tabs/portfolio.tsx
+++ app/components/tabs/portfolio.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import UploadCta from "../uploadcta";
 import { uploadPortfolio } from "@/app/lib/user";
+import { EllipsisVertical } from "lucide-react";
 
 interface Props {
   user_id: string;
@@ -37,6 +38,20 @@ const Portfolio = ({ user_id }: Props) => {
             Upload Portfolio
           </button>
         </div>
+      </div>
+      <div></div>
+      <div className="w-full p-6 border border-[color:#DBDBDB] rounded-md">
+        <span className="flex justify-between items-center">
+          <p className="text-sm font-medium">Post Impressions</p>
+          <div role="tablist" className="tabs tabs-boxed tabs-xs">
+            <a role="tab" className="tab tab-active">
+              Monthly
+            </a>
+            <a role="tab" className="tab">
+              Daily
+            </a>
+          </div>
+        </span>
       </div>
     </div>
   );
