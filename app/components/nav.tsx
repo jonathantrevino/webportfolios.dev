73fd0715bad9128ab57494e05cbc3d419405437d
yeahimjt <JonathanTrevino1327@yahoo.com>
2024-09-27 13:51:03 -0500

    updating nav home

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -12,7 +12,10 @@ const Nav = () => {
     <nav className="p-[25px] flex justify-between items-center bg-[color:#FAFAFA] border boder-[color:#DBDBDB]">
       <div className="flex gap-[30px] items-center h-[40px]">
         <Link href="/" className="flex gap-[10px]">
-          <div className="font-medium text-lg">webportfolios.dev</div>
+          <div className="font-medium text-lg">
+            webportfolios
+            <span className="font-semibold text-primary">.dev</span>
+          </div>
         </Link>
         <ul className="hidden gap-[20px] text-[color:#363636] sm:flex">
           <li className="hover:text-[color:#000000]">
