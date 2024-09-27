fa46c1aa3470b4b819a7ed26348ea8c6c7745ee4
yeahimjt <JonathanTrevino1327@yahoo.com>
2024-09-26 23:42:46 -0500

    adding portfolios page

---
+++ app/portfolios/page.tsx
@@ -0,0 +1,85 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import Nav from "../components/nav";
+import { PortfolioType } from "@/types";
+import { paginatePortfolios } from "../lib/user";
+import Image from "next/image";
+import { Eye, LinkIcon } from "lucide-react";
+import Profile from "../components/profile";
+
+const page = () => {
+  const [portfolios, setPortfolios] = useState<PortfolioType[] | null>(null);
+  useEffect(() => {
+    handlePortfolios();
+  }, []);
+  async function handlePortfolios() {
+    const response = await paginatePortfolios(null);
+    if (!response) return;
+    setPortfolios(response);
+  }
+  return (
+    <>
+      <Nav />
+      <main className="flex break:flex-row max-w-[1440px] w-3/4 mx-auto flex-col  gap-10 lg:gap-0">
+        <section className="pt-24 w-full grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
+          {portfolios &&
+            portfolios.map((portfolio: PortfolioType) => (
+              <div className="space-y-3">
+                <div className="relative">
+                  <div className="w-full h-fit aspect-video border border-[color:#DBDBDB] relative rounded-md">
+                    <Image
+                      className="rounded-md"
+                      src={portfolio?.photoURL[0]}
+                      fill
+                      draggable={false}
+                      alt="portfolio preview"
+                    />
+                  </div>
+
+                  <div className="absolute bottom-4 right-4 flex gap-2">
+                    <div className="tooltip" data-tip="View Post">
+                      <a
+                        role="button"
+                        href={`/portfolios/${portfolio.user_id}`}
+                        className="btn btn-sm border border-base-300 shadow-sm"
+                      >
+                        <Eye />
+                      </a>
+                    </div>
+                    <div className="tooltip" data-tip="View Portfolio">
+                      <a
+                        role="button"
+                        target="_blank"
+                        href={portfolio?.portfolioURL}
+                        className="btn btn-sm"
+                      >
+                        <LinkIcon />
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                <div className="flex items-center gap-4">
+                  <Image
+                    src={portfolio.user_photoURL!}
+                    className="rounded-full"
+                    width={32}
+                    height={32}
+                    draggable={false}
+                    alt="portfolio owner"
+                  />
+                  <div className="">
+                    <h1 className="text-sm font-semibold">
+                      {portfolio.user_displayName!}
+                    </h1>
+                    <p className="text-xs">{portfolio.user_title!}</p>
+                  </div>
+                </div>
+              </div>
+            ))}
+        </section>
+      </main>
+    </>
+  );
+};
+
+export default page;
