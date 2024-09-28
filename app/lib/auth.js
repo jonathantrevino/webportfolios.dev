f8433d18da9e246730d9ac4d1bc573311edf20b7
yeahimjt <JonathanTrevino1327@yahoo.com>
2024-09-27 20:13:44 -0500

    updating callback url for signin

--- app/lib/auth.js
+++ app/lib/auth.js
@@ -5,7 +5,7 @@ import { doc, updateDoc } from "firebase/firestore";
 const actionCodeSettings = {
   // URL you want to redirect back to. The domain (www.example.com) for this
   // URL must be in the authorized domains list in the Firebase Console.
-  url: "http://localhost:3000/magic-link-confirmation",
+  url: "http://www.webportfolios.dev/magic-link-confirmation",
   // This must be true.
   handleCodeInApp: true,
   // iOS: {
