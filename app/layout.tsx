87fbf10bd2c0f6851110f118260868a420480a2e
yeahimjt <JonathanTrevino1327@yahoo.com>
2024-10-02 21:09:52 -0500

    adding favicon declaration

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,6 +42,8 @@ export default function RootLayout({
           sizes="16x16"
           href="/favicon-16x16.png"
         />
+
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta property="og:image" content="/opengraph-image.png" />
