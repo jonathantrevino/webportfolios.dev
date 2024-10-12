1783a87d47ac7e5afa78acdb37f5d1837cfb99ac
yeahimjt <JonathanTrevino1327@yahoo.com>
2024-10-11 20:56:14 -0500

    fixing condition

--- app/components/tabs/portfolio.tsx
+++ app/components/tabs/portfolio.tsx
@@ -118,7 +118,6 @@ const Portfolio = ({ user_id }: Props) => {
         <div className="flex justify-end">
           <button
             className="btn btn-primary btn-sm"
-            disabled={!portfolioTemp || !validURL.test(portfolioTemp)}
             onClick={() => handleUploadPortfolio()}
           >
             Upload Portfolio
