1e4fb37a06827282e80068f5595c8756fb759122
yeahimjt <JonathanTrevino1327@yahoo.com>
2024-10-19 20:02:55 -0500

    making max-w global

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,7 +49,7 @@ export default function RootLayout({
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
       </head>
-      <body className={inter.className}>
+      <body className={inter.className + ' max-w-[1440px] mx-auto'}>
         {children}
         <SpeedInsights />
         <Analytics />
