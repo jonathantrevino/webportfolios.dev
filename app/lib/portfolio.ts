45f1306cb13596780cb30c8e66ed14b3c11a4e37
yeahimjt <JonathanTrevino1327@yahoo.com>
2024-10-19 20:01:56 -0500

    new portfolio functions

---
+++ app/lib/portfolio.ts
@@ -0,0 +1,54 @@
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  startAfter,
+} from "firebase/firestore";
+import { firestore, storage } from "./firebase";
+
+export async function getRecentPortfolios(lastVisible: any) {
+  const portfolioRef = collection(firestore, 'portfolios');
+  let portfolioQuery = query(
+    portfolioRef,
+    orderBy('uploaded', 'desc'),
+    limit(5),
+  )
+  if (lastVisible) {
+    portfolioQuery = query(portfolioRef, orderBy('uploaded', 'desc'), startAfter(lastVisible), limit(3))
+  }
+
+  const querySnapshot = await getDocs(portfolioQuery)
+  if (!querySnapshot.empty) {
+    const portfolios = await Promise.all(
+      querySnapshot.docs.map(async (docItem) => {
+        const portfolioData = docItem.data();
+        const userId = portfolioData.user_id;
+
+        // Fetch user information from Firestore
+        const userDocRef = doc(firestore, 'users', userId); // Assume users collection
+        const userDocSnapshot = await getDoc(userDocRef);
+
+        let userData = {};
+        if (userDocSnapshot.exists()) {
+          userData = userDocSnapshot.data();
+        }
+
+        return {
+          ...portfolioData,
+          user_displayName: userData.displayName,
+          user_photoURL: userData.photoURL,
+          user_title: userData.title,
+        };
+      })
+    );
+    return portfolios
+  }
+
+
+
+
+}
