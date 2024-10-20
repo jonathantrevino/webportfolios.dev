14667b2bf32a51bb1c6ecd7b37900ff14b65a902
jonathantrevino <JonathanTrevino1327@yahoo.com>
2024-10-19 21:58:53 -0500

    removing hard coded view more

--- app/components/recentPortfolios.tsx
+++ app/components/recentPortfolios.tsx
@@ -13,7 +13,7 @@ const RecentPortfolios = ({ portfoliosList }: { portfoliosList: PortfolioType[] 
       {portfoliosList && portfoliosList.length > 0 && (
         <li>
           <Link href='/portfolios' className='w-full min-h-[44px] h-[calc(100%-44px)] bg-primary-content/50 border border-primary/50 text-primary rounded-lg flex justify-center items-center space-x-2 relative transition-all ease-in-out hover:shadow-md group'>
-            <p className='group-hover:scale-[1.05] delay-100 ease-in transition-all'>View 127 More</p>        </Link>
+            <p className='group-hover:scale-[1.05] delay-100 ease-in transition-all'>View More</p>        </Link>
         </li>
       )}
     </ul >
