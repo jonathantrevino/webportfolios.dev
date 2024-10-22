e3fdda33136a252575aa1e8d7c123d0498a4f995
jonathantrevino <JonathanTrevino1327@yahoo.com>
2024-10-21 19:16:18 -0500

    unchanged

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,7 +42,6 @@ export default function RootLayout({
           sizes="16x16"
           href="/favicon-16x16.png"
         />
-
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta property="og:image" content="/opengraph-image.png" />
